6453afb358cf532b296b0336323eee6d65a3cf6d
Fahim <contact@fkds.fr>
2026-01-05 18:33:39 +0100

    cuda12.6 needen for 1080ti

--- prep_ubuntu.sh
+++ prep_ubuntu.sh
@@ -3,4 +3,5 @@ source bench_venv/bin/activate
 
 # Upgrade pip and install dependencies
 python -m pip install --upgrade pip
-python -m pip install torch numpy
+python -m pip install numpy
+python -m pip install torch --extra-index-url https://download.pytorch.org/whl/cu126
